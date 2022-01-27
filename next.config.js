7fb3683d42601bc858f6af71b6db5ea16a3b4929
Jirka Svoboda <svobik7@gmail.com>
2022-01-27 16:21:46 +0100

    Fixed well-known json serve

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        destination: '/.well-known/apple-app-site-association.json',
+      },
+    ];
+  },
+});
